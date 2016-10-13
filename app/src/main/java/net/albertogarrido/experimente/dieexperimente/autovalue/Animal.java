ee74030a3a5ac9ff3f6674053fe569ac29426be7
albertogarrido <hello@albertogarrido.net>
2016-10-13 10:15:56 +0200

    Initial project setup, add AutoValue lib

---
+++ app/src/main/java/net/albertogarrido/experimente/dieexperimente/autovalue/Animal.java
@@ -0,0 +1,11 @@
+package net.albertogarrido.experimente.dieexperimente.autovalue;
+
+import com.google.auto.value.AutoValue;
+
+/**
+ * Created by agarrido on 13/10/16.
+ */
+
+@AutoValue
+public abstract class Animal {
+}
